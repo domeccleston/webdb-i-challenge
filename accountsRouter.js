f2ef59bbea69ac4b6053dc74fe0d7848fba9f8c7
Dom <domeccleston@gmail.com>
2019-11-04 14:03:00 +0000

    stretch done

--- accountsRouter.js
+++ accountsRouter.js
@@ -4,49 +4,68 @@ const server = require("./server");
 const accountsRouter = express.Router();
 
 accountsRouter.get("/", (req, res) => {
-    db('accounts')
-     .then(accounts => {
-         res.status(200).json(accounts);
-     })
-     .catch(err => {
-         res.status(500).json(err);
-     })
+  if (req.query) {
+    db("accounts")
+      .limit(req.query.limit)
+      .orderBy(req.query.sortBy || 'id', req.query.sortDir || 'desc')
+      .then(accounts => {
+        res.status(200).json(accounts);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  } else {
+  db("accounts")
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+}
 });
 
 accountsRouter.get("/:id", async (req, res) => {
-    try {
-        const response = await db('accounts').where({ id: req.params.id });
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json(error);
-    }
+  try {
+    const response = await db("accounts").where({ id: req.params.id });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 accountsRouter.post("/", async (req, res) => {
-    try {
-        const newAccount = await db('accounts').insert({ name: req.body.name, budget: req.body.budget });
-        res.status(200).json(newAccount);
-    } catch (error) {
-        res.status(500).json({ message: "Error processing request: " + error})
-    }
-})
+  try {
+    const newAccount = await db("accounts").insert({
+      name: req.body.name,
+      budget: req.body.budget
+    });
+    res.status(200).json(newAccount);
+  } catch (error) {
+    res.status(500).json({ message: "Error processing request: " + error });
+  }
+});
 
 accountsRouter.put("/:id", async (req, res) => {
-    try {
-        const updatedAccount = await db('accounts').where({ id: req.params.id }).update(req.body);
-        res.status(200).json(updatedAccount);
-    } catch (error) {
-        res.status(500).json({ message: "Error processing request: " + error});
-    }
-})
+  try {
+    const updatedAccount = await db("accounts")
+      .where({ id: req.params.id })
+      .update(req.body);
+    res.status(200).json(updatedAccount);
+  } catch (error) {
+    res.status(500).json({ message: "Error processing request: " + error });
+  }
+});
 
 accountsRouter.delete("/:id", async (req, res) => {
-    try {
-        const deleted = await db('accounts').where({ id: req.params.id }).del();
-        res.status(200).json({ message: "Deleted " + deleted + " accounts." })
-    } catch (error) {
-        res.status(500).json({ message: "Error processing request: " + error})
-    }
-})
+  try {
+    const deleted = await db("accounts")
+      .where({ id: req.params.id })
+      .del();
+    res.status(200).json({ message: "Deleted " + deleted + " accounts." });
+  } catch (error) {
+    res.status(500).json({ message: "Error processing request: " + error });
+  }
+});
 
 module.exports = accountsRouter;
